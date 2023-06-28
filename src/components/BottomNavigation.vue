dc8cf8d9250e731be0650319fed3e7ccacbba6fa
Abdullah Masulili <abdullah.masulili@gmail.com>
2023-06-28 23:56:59 +0800

    feat(BottomNavigation): add bottom navigation

---
+++ src/components/BottomNavigation.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { useAppStore } from '@/store/app';
+
+const app = useAppStore()
+</script>
+
+<template>
+    <v-bottom-navigation class="d-flex d-md-none">
+        <v-btn value="/library">
+            <v-icon>mdi-library-shelves</v-icon>
+            Pokemon Library
+        </v-btn>
+        <v-btn value="/favourite">
+            <v-icon>mdi-pokeball</v-icon>
+            Your Pokemon
+        </v-btn>
+        <v-btn value="/filter" @click="app.toggleDrawer('filter')">
+            <v-icon>mdi-filter</v-icon>
+            Filter
+        </v-btn>
+    </v-bottom-navigation>
+</template>
